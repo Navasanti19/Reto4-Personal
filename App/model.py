c6c2b6e4ba632f13ba3e98c9b0c099c09d983ab1
Santiago <navasanti19@gmail.com>
2022-11-24 08:38:15 -0500

    Plantilla pro

--- App/model.py
+++ App/model.py
@@ -1,5 +1,5 @@
 ﻿"""
- * Copyright 2020, Departamento de sistemas y Computación
+ * Copyright 2020, Departamento de sistemas y Computación,
  * Universidad de Los Andes
  *
  *
@@ -17,343 +17,97 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * Contribución de:
+ * along withthis program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Dario Correal
+ * Contribuciones:
  *
+ * Dario Correal - Version inicial
+ Siuu
  """
-import config
-from DISClib.ADT import graph as gr
-from DISClib.ADT import map as m
+
+
+from csv import list_dialects
+import math
+import config as cf
+import time
+import folium
+from tabulate import tabulate
+from folium.plugins import MarkerCluster
+from datetime import datetime
+import tracemalloc
 from DISClib.ADT import list as lt
-from DISClib.Algorithms.Graphs import scc
-from DISClib.Algorithms.Graphs import dijsktra as djk
-from DISClib.Algorithms.Graphs import dfs
-from DISClib.Algorithms.Graphs import bfs
-from DISClib.Utils import error as error
-assert config
+from DISClib.ADT import map as mp
+from DISClib.ADT import orderedmap as om
+from DISClib.DataStructures import mapentry as me
+from DISClib.Algorithms.Sorting import shellsort as sa
+from DISClib.Algorithms.Sorting import mergesort as mer
+assert cf
 
 """
-En este archivo definimos los TADs que vamos a usar y las operaciones
-de creacion y consulta sobre las estructuras de datos.
+Se define la estructura de un catálogo de videos. El catálogo tendrá dos listas, una para los videos, otra para las categorias de
+los mismos.
 """
 
-# -----------------------------------------------------
-#                       API
-# -----------------------------------------------------
+# Construccion de modelos
 
-
-def newAnalyzer():
-    """ Inicializa el analizador
-
-   stops: Tabla de hash para guardar los vertices del grafo
-   connections: Grafo para representar las rutas entre estaciones
-   components: Almacena la informacion de los componentes conectados
-   paths: Estructura que almancena los caminos de costo minimo desde un
-           vertice determinado a todos los otros vértices del grafo
+def newCatalog():
     """
-    try:
+    Inicializa el catálogo de libros. Crea una lista vacia para guardar
+    todos los libros, adicionalmente, crea una lista vacia para los autores,
+    una lista vacia para los generos y una lista vacia para la asociación
+    generos y libros. Retorna el catalogo inicializado.
+    """
     
-
-        analyzer = {
-            'stops': None,
-            'connections': None,
-            'components': None,
-            'paths': None,
-            'search': None
-            
-        }
-
-        
-        analyzer['stops'] = m.newMap(numelements=14000,
-                                     maptype='PROBING',
-                                     comparefunction=compareStopIds)
-
-        analyzer['connections'] = gr.newGraph(datastructure='ADJ_LIST',
-                                              directed=True,
-                                              size=14000,
-                                              comparefunction=compareStopIds)
-        return analyzer
-    except Exception as exp:
-        error.reraise(exp, 'model:newAnalyzer')
+    catalog = {}
 
 
-# Funciones para agregar informacion al grafo
+    return catalog
 
-def addStopConnection(analyzer, lastservice, service):
-    """
-    Adiciona las estaciones al grafo como vertices y arcos entre las
-    estaciones adyacentes.
-
-    Los vertices tienen por nombre el identificador de la estacion
-    seguido de la ruta que sirve.  Por ejemplo:
-
-    75009-10
-
-    Si la estacion sirve otra ruta, se tiene: 75009-101
-    """
-    try:
-        origin = formatVertex(lastservice)
-        destination = formatVertex(service)
-        cleanServiceDistance(lastservice, service)
-        distance = float(service['Distance']) - float(lastservice['Distance'])
-        distance = abs(distance)
-        addStop(analyzer, origin)
-        addStop(analyzer, destination)
-        addConnection(analyzer, origin, destination, distance)
-        addRouteStop(analyzer, service)
-        addRouteStop(analyzer, lastservice)
-        return analyzer
-    except Exception as exp:
-        error.reraise(exp, 'model:addStopConnection')
+# Funciones para agregar informacion al catalogo
 
 
-def addStop(analyzer, stopid):
-    """
-    Adiciona una estación como un vertice del grafo
-    """
-    try:
-        if not gr.containsVertex(analyzer['connections'], stopid):
-            gr.insertVertex(analyzer['connections'], stopid)
-        return analyzer
-    except Exception as exp:
-        error.reraise(exp, 'model:addstop')
-
-
-def addRouteStop(analyzer, service):
-    """
-    Agrega a una estacion, una ruta que es servida en ese paradero
-    """
-    entry = m.get(analyzer['stops'], service['BusStopCode'])
-    if entry is None:
-        lstroutes = lt.newList(cmpfunction=compareroutes)
-        lt.addLast(lstroutes, service['ServiceNo'])
-        m.put(analyzer['stops'], service['BusStopCode'], lstroutes)
-    else:
-        lstroutes = entry['value']
-        info = service['ServiceNo']
-        if not lt.isPresent(lstroutes, info):
-            lt.addLast(lstroutes, info)
-    return analyzer
-
-
-def addRouteConnections(analyzer):
-    """
-    Por cada vertice (cada estacion) se recorre la lista
-    de rutas servidas en dicha estación y se crean
-    arcos entre ellas para representar el cambio de ruta
-    que se puede realizar en una estación.
-    """
-    lststops = m.keySet(analyzer['stops'])
-    for key in lt.iterator(lststops):
-        lstroutes = m.get(analyzer['stops'], key)['value']
-        prevrout = None
-        for route in lt.iterator(lstroutes):
-            route = key + '-' + route
-            if prevrout is not None:
-                addConnection(analyzer, prevrout, route, 0)
-                addConnection(analyzer, route, prevrout, 0)
-            prevrout = route
-
-
-def addConnection(analyzer, origin, destination, distance):
-    """
-    Adiciona un arco entre dos estaciones
-    """
-    edge = gr.getEdge(analyzer['connections'], origin, destination)
-    if edge is None:
-        gr.addEdge(analyzer['connections'], origin, destination, distance)
-    return analyzer
-
-# ==============================
 # Funciones de consulta
-# ==============================
 
+def getReq1():
+    pass
 
-def connectedComponents(analyzer):
-    """
-    Calcula los componentes conectados del grafo
-    Se utiliza el algoritmo de Kosaraju
-    """
-    analyzer['components'] = scc.KosarajuSCC(analyzer['connections'])
-    return scc.connectedComponents(analyzer['components'])
+def getReq2 ():
+    pass
 
+def getReq3():
+    pass
 
-def minimumCostPaths(analyzer, initialStation):
-    """
-    Calcula los caminos de costo mínimo desde la estacion initialStation
-    a todos los demas vertices del grafo
-    """
-    analyzer['paths'] = djk.Dijkstra(analyzer['connections'], initialStation)
-    return analyzer
+def getReq4():
+    pass
 
+def getReq5():
+    pass
 
-def hasPath(analyzer, destStation):
-    """
-    Indica si existe un camino desde la estacion inicial a la estación destino
-    Se debe ejecutar primero la funcion minimumCostPaths
-    """
-   
-    return djk.hasPathTo(analyzer['paths'], destStation)
+def getReq6():
+    pass
 
+def getReq7():
+    pass
 
-def minimumCostPath(analyzer, destStation):
-    """
-    Retorna el camino de costo minimo entre la estacion de inicio
-    y la estacion destino
-    Se debe ejecutar primero la funcion minimumCostPaths
-    """
-    path = djk.pathTo(analyzer['paths'], destStation)
-    return path
-
-
-def searchPaths(analyzer, initialStation, method):
-    """
-    searchPaths Calcula los caminos posibles desde una estacion de origen
-    y puede utilizar los algoritmos "dfs" o "bfs"
-
-    Args:
-        analyzer (dict): diccionario con las estructuras de datos del modelo
-        originStation (vertice): estacion de origen del recorrido
-        method (str, optional): algoritmo de busqueda. Por defecto es "dfs"
-
-    Returns:
-        dict: devuelve el analyzer del modelo
-    """
-   
-    if method == "dfs":
-        analyzer["search"]=dfs.DepthFirstSearch(analyzer["connections"], initialStation)
-        
-    elif method == "bfs":
-        analyzer["search"]=bfs.BreadhtFisrtSearch(analyzer["connections"], initialStation)
-    return analyzer
-
-
-def hasSearchPath(analyzer, destStation, method):
-    """
-    hasSearchPath indica si existe un camino desde la estacion inicial a
-    la estación destino. Se debe ejecutar primero la funcion searchPaths()
-
-    Args:
-        analyzer (dict): diccionario con las estructuras de datos del modelo
-        destStation (vertice): estacion de destino para el recorrido
-        method (str, optional): algoritmo de busqueda. Por defecto es "dfs"
-    """
-   
-    if method == "dfs":
-        print(destStation)
-        x=dfs.hasPathTo(analyzer['search'], destStation)
-        return x
-    elif method == "bfs":
-        x= bfs.hasPathTo(analyzer['search'], destStation)
-        return x
-
-def searchPathTo(analyzer, destStation, method):
-    """
-    searchPath retorna el camino de encontrado entre la estacion de inicio
-    y la estacion destino Se debe ejecutar primero la funcion searchPaths
-
-    Args:
-        analyzer (dict): diccionario con las estructuras de datos del modelo
-        destStation (vertice): estacion de destino para el recorrido
-        method (str, optional): algoritmo de busqueda. Por defecto es "dfs"
-
-    Returns:
-        stack: devuele una pila con el camino encontrado en la busqueda.
-    """
-    path = None
+def getReq8():
+    pass
     
-    if method == "dfs":
-        path= dfs.pathTo(analyzer['search'],destStation)
-    
-    elif method == "bfs":
-        path= bfs.pathTo(analyzer['search'],destStation)
-    return path
+
+# Funciones de comparación
 
 
-def totalStops(analyzer):
+
+#Funciones de Tiempo
+
+def getTime():
     """
-    Retorna el total de estaciones (vertices) del grafo
+    devuelve el instante tiempo de procesamiento en milisegundos
     """
-    return gr.numVertices(analyzer['connections'])
+    return float(time.perf_counter()*1000)
 
-
-def totalConnections(analyzer):
+def deltaTime(start, end):
     """
-    Retorna el total arcos del grafo
+    devuelve la diferencia entre tiempos de procesamiento muestreados
     """
-    return gr.numEdges(analyzer['connections'])
-
-
-def servedRoutes(analyzer):
-    """
-    Retorna la estación que sirve a mas rutas.
-    Si existen varias rutas con el mismo numero se
-    retorna una de ellas
-    """
-    lstvert = m.keySet(analyzer['stops'])
-    maxvert = None
-    maxdeg = 0
-    for vert in lt.iterator(lstvert):
-        lstroutes = m.get(analyzer['stops'], vert)['value']
-        degree = lt.size(lstroutes)
-        if(degree > maxdeg):
-            maxvert = vert
-            maxdeg = degree
-    return maxvert, maxdeg
-
-
-# ==============================
-# Funciones Helper
-# ==============================
-
-def cleanServiceDistance(lastservice, service):
-    """
-    En caso de que el archivo tenga un espacio en la
-    distancia, se reemplaza con cero.
-    """
-    if service['Distance'] == '':
-        service['Distance'] = 0
-    if lastservice['Distance'] == '':
-        lastservice['Distance'] = 0
-
-
-def formatVertex(service):
-    """
-    Se formatea el nombrer del vertice con el id de la estación
-    seguido de la ruta.
-    """
-    name = service['BusStopCode'] + '-'
-    name = name + service['ServiceNo']
-    return name
-
-
-# ==============================
-# Funciones de Comparacion
-# ==============================
-
-
-def compareStopIds(stop, keyvaluestop):
-    """
-    Compara dos estaciones
-    """
-    stopcode = keyvaluestop['key']
-    if (stop == stopcode):
-        return 0
-    elif (stop > stopcode):
-        return 1
-    else:
-        return -1
-
-
-def compareroutes(route1, route2):
-    """
-    Compara dos rutas
-    """
-    if (route1 == route2):
-        return 0
-    elif (route1 > route2):
-        return 1
-    else:
-        return -1
+    elapsed = float(end - start)
+    return elapsed
