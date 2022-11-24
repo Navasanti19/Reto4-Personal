f3a95aff50201358b52c1712dcf57ca5348bcff4
Santiago <navasanti19@gmail.com>
2022-11-24 08:34:40 -0500

    Plantilla

--- App/model.py
+++ App/model.py
@@ -18,33 +18,138 @@
  *
  * You should have received a copy of the GNU General Public License
  * along withthis program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Contribuciones:
- *
- * Dario Correal - Version inicial
  """
 
-
+import tracemalloc
 import config as cf
-from DISClib.ADT import list as lt
-from DISClib.ADT import map as mp
-from DISClib.DataStructures import mapentry as me
-from DISClib.Algorithms.Sorting import shellsort as sa
-assert cf
+import model
+import csv
+import time
+import tracemalloc
+
+csv.field_size_limit(2147483647)
 
 """
-Se define la estructura de un catálogo de videos. El catálogo tendrá dos listas, una para los videos, otra para las categorias de
-los mismos.
+El controlador se encarga de mediar entre la vista y el modelo.
 """
 
-# Construccion de modelos
+# Inicialización del Catálogo de juegos
 
-# Funciones para agregar informacion al catalogo
+def newController():
+    """
+    Crea una instancia del modelo
+    """
+    control = {
+        'model': None
+    }
+    control['model'] = model.newCatalog()
+    return control
 
-# Funciones para creacion de datos
 
-# Funciones de consulta
+# Funciones para la carga de datos
 
-# Funciones utilizadas para comparar elementos dentro de una lista
+def loadData(control,archiv, memory = False):
+    start_time = getTime()
+    if memory:
+        tracemalloc.start()
+        start_memory = getMemory()
 
-# Funciones de ordenamiento
+    catalog = control['model']
+    juegos= loadJuegos(catalog,archiv)
+    record = loadRecords(catalog,archiv)
+    loadPaises(catalog)
+
+    stop_time = getTime()
+    delta_time = deltaTime(stop_time, start_time)
+    if memory:
+        stop_memory = getMemory()
+        tracemalloc.stop()
+        stop_time = getTime()
+        delta_memory = deltaMemory(stop_memory, start_memory)
+        return juegos, record, delta_time, delta_memory
+
+    else:
+        return juegos, record, delta_time,None
+
+def loadJuegos(catalog,archiv):
+    booksfile = cf.data_dir + 'game_data_utf-8-'+archiv
+    input_file = csv.DictReader(open(booksfile, encoding='utf-8'))
+    for juego in input_file:
+        model.addJuego(catalog, juego)
+    return catalog['juegos']
+
+def loadRecords(catalog,archiv):
+    booksfile = cf.data_dir + 'category_data_utf-8-'+archiv
+    input_file = csv.DictReader(open(booksfile, encoding='utf-8'))
+    for juego in input_file:
+        model.addRecord(catalog, juego,juego['Game_Id'])
+    return catalog['records']
+
+def loadPaises(catalog):
+    booksfile = cf.data_dir + 'paises_2016_geom_10.csv'
+    input_file = csv.DictReader(open(booksfile, encoding='utf-8'))
+    for juego in input_file:
+        model.addPais(catalog, juego)
+    
+
+# Funciones de consulta sobre el catálogo
+
+def getReq1():
+    pass
+
+def getReq2():
+    pass
+
+def getReq3():
+    pass
+
+def getReq4():
+    pass
+
+def getReq5():
+    pass
+
+def getReq6():
+    pass
+
+def getReq7():
+    pass
+
+def getReq8():
+    pass
+
+# Funciones de tiempo
+
+def getTime():
+    """
+    devuelve el instante tiempo de procesamiento en milisegundos
+    """
+    return float(time.perf_counter()*1000)
+
+def deltaTime(end, start):
+    """
+    devuelve la diferencia entre tiempos de procesamiento muestreados
+    """
+    elapsed = float(end - start)
+    return elapsed
+
+def getMemory():
+    """
+    toma una muestra de la memoria alocada en instante de tiempo
+    """
+    return tracemalloc.take_snapshot()
+
+def deltaMemory(stop_memory, start_memory):
+    """
+    calcula la diferencia en memoria alocada del programa entre dos
+    instantes de tiempo y devuelve el resultado en bytes (ej.: 2100.0 B)
+    """
+    memory_diff = stop_memory.compare_to(start_memory, "filename")
+    delta_memory = 0.0
+
+    # suma de las diferencias en uso de memoria
+    for stat in memory_diff:
+        delta_memory = delta_memory + stat.size_diff
+    # de Byte -> kByte
+    delta_memory = delta_memory/1024.0
+    return delta_memory
--- App/view.py
+++ App/view.py
@@ -19,7 +19,7 @@
  * You should have received a copy of the GNU General Public License
  * along withthis program.  If not, see <http://www.gnu.org/licenses/>.
  """
-
+import os
 import config as cf
 import sys
 import controller
@@ -34,12 +34,109 @@ se hace la solicitud al controlador para ejecutar la
 operación solicitada
 """
 
+# Funciones de Print
+
 def printMenu():
     print("Bienvenido")
     print("1- Cargar información en el catálogo")
     print("2- ")
 
-catalog = None
+# Función crear controlador
+
+def newController():
+    control = controller.newController()
+    return control
+
+# Función Cargar Datos
+
+def loadData(control,archiv,memory):
+    movies= controller.loadData(control,archiv,memory)
+    return movies
+
+
+# Funciones para ejecutar el menú
+def playLoadData():
+    print('\nCuántos datos desea cargar?')
+    print('1: 0.5% de los datos')
+    print('2: 5% de los datos')
+    print('3: 10% de los datos')
+    print('4: 20% de los datos')
+    print('5: 30% de los datos')
+    print('6: 50% de los datos')
+    print('7: 80% de los datos')
+    print('8: 100% de los datos')
+    resp=int(input())
+    if resp==1:
+        archiv='small.csv'
+    elif resp==2:
+        archiv='5pct.csv'
+    elif resp==3:
+        archiv='10pct.csv'
+    elif resp==4:
+        archiv='20pct.csv'
+    elif resp==5:
+        archiv='30pct.csv'
+    elif resp==6:
+        archiv='50pct.csv'
+    elif resp==7:
+        archiv='80pct.csv'
+    elif resp==8:
+        archiv='large.csv'
+    
+    resp=input(('\nDesea Conocer la memoria utilizada? '))
+    resp=castBoolean(resp)
+    juegos,records,time,memory= loadData(catalog,archiv,resp)
+    os.system('cls')
+    print('----------------------------------')
+    print('Loaded speedruning data properties: ')
+    print('Total loaded games: '+str(lt.size(juegos)))
+    print('Total loaded speedruns: '+str(lt.size(records)))
+    print('----------------------------------')
+    
+    print('\n------ Game Content ------')   
+    head=['Game_Id','Release_Date',"Name",'Abbreviation','Platforms','Total_Runs','Genres']
+    #printMoviesCant(juegos,3,head)
+    
+    print('\n------ SpeedRuns Content ------')   
+    head=['Game_Id','Record_Date_0','Num_Runs',"Name",'Category','Subcategory','Country_0','Players_0','Time_0']
+    #printMoviesCant(records,3,head)
+    print(f'Tiempo de ejecución: {time:.3f}')
+    print(f'Memoria Utilizada: {memory}\n')
+
+def playReq1():
+    pass
+
+def playReq2():
+    pass
+
+def playReq3():
+    pass
+
+def playReq4():
+    pass
+
+def playReq5():
+   pass
+
+def playReq6():
+    pass
+
+def playReq7():
+    pass
+
+def playReq8():
+    pass
+
+# Funciones Auxiliares
+
+def castBoolean(value):
+    """
+    Convierte un valor a booleano
+    """
+    if value in ('True', 'true', 'TRUE', 'T', 't', '1', 1, True):
+        return True
+    else:
+        return False
 
 """
 Menu principal
@@ -47,12 +144,26 @@ Menu principal
 while True:
     printMenu()
     inputs = input('Seleccione una opción para continuar\n')
-    if int(inputs[0]) == 1:
+    if int(inputs[0]) == 0:
         print("Cargando información de los archivos ....")
-
-    elif int(inputs[0]) == 2:
-        pass
-
+        catalog = newController()
+        playLoadData() 
+    elif int(inputs[0])==1:
+        playReq1()
+    elif int(inputs[0])==2:
+        playReq2()
+    elif int(inputs[0])==3:
+        playReq3()
+    elif int(inputs[0])==4:
+        playReq4()
+    elif int(inputs[0])==5:
+        playReq5()
+    elif int(inputs[0])==6:
+        playReq6()
+    elif int(inputs[0])==7:
+        playReq7()
+    elif int(inputs[0])==8:
+        playReq8() 
     else:
         sys.exit(0)
-sys.exit(0)
+
